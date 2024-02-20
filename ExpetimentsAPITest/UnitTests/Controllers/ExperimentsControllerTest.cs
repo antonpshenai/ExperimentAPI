40c6bb584eb052a576cf4a18243f8a522b5a9df0
Anton <ayaponec1995@gmail.com>
2024-02-20 19:20:13 +0200

    added unit tests

---
+++ ExpetimentsAPITest/UnitTests/Controllers/ExperimentsControllerTest.cs
@@ -0,0 +1,54 @@
+﻿using ExperementsAPI.Controllers;
+using ExperementsAPI.DTO;
+using ExperementsAPI.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace ExpetimentsAPITest.UnitTests.Controllers
+{
+    public class ExperimentsControllerTest
+    {
+        private ExperimentsController _controller;
+        private Mock<IConfiguration> _mockConfiguration;
+        private Mock<ILogger<ExperimentsController>> _mockLogger;
+        private Mock<IExperimentService> _mockExperimentService;
+
+        public ExperimentsControllerTest()
+        {
+            _mockLogger = new Mock<ILogger<ExperimentsController>>();
+            _mockConfiguration = new Mock<IConfiguration>();
+            _mockExperimentService = new Mock<IExperimentService>();
+
+            _controller = new ExperimentsController(_mockLogger.Object, _mockExperimentService.Object, _mockConfiguration.Object);
+        }
+
+        [Fact]
+        public async Task GetExperimentWithValidEndpoint()
+        {
+            _mockConfiguration.Setup(config => config.GetSection("ExperimentEndpoints")["button-color"]).Returns("button_color");
+            _mockExperimentService.Setup(service => service.GetExperimentResponseAsync(It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(new ResponseDTO());
+
+            var result = await _controller.GetExperiment("deviceToken", "button-color");
+
+            Assert.IsType<OkObjectResult>(result);
+            var okResult = result as OkObjectResult;
+            Assert.IsType<ResponseDTO>(okResult?.Value);
+        }
+
+        [Fact]
+        public async Task GetExperimentWithNotValidEndpoint()
+        {
+            _mockConfiguration.Setup(config => config.GetSection("ExperimentEndpoints")["cats"]).Returns((string)null);
+
+            var result = await _controller.GetExperiment("deviceToken", "cats");
+
+            Assert.IsType<NotFoundObjectResult>(result);
+            var notFoundResult = result as NotFoundObjectResult;
+            Assert.Equal($"Experiment endpoint with name 'cats' not found.", notFoundResult?.Value);
+        }
+
+    }
+}
---
+++ ExpetimentsAPITest/UnitTests/Services/ExperimentServiceTest.cs
@@ -0,0 +1,76 @@
+﻿using ExperementsAPI.DataBase;
+using ExperementsAPI.DTO;
+using ExperementsAPI.Model;
+using ExperementsAPI.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpetimentsAPITest.UnitTests.Services
+{
+    public class ExperimentServiceTest
+    {
+        private readonly DbContextOptions<AppDBContext> _dbContextOptions;
+
+        public ExperimentServiceTest()
+        {
+            _dbContextOptions = new DbContextOptionsBuilder<AppDBContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+        }
+
+        [Fact]
+        public async Task GetExperimentResponseAsyncReturnsResponseDTO()
+        {
+            using (var context = new AppDBContext(_dbContextOptions))
+            {
+                var service = new ExperimentService(context);
+
+                var result = await service.GetExperimentResponseAsync("newDeviceToken", "experimentName");
+
+                Assert.IsType<ResponseDTO>(result);
+                Assert.NotNull(result.Key);
+                Assert.NotNull(result.Value);
+            }
+        }
+
+        [Fact]
+        public void GetExperimentStatisticsReturnsExperimentStatisticsDto()
+        {
+            using (var context = new AppDBContext(_dbContextOptions))
+            {
+                var service = new ExperimentService(context);
+                SampleDatabase(context);
+
+                var result = service.GetExperimentStatistics();
+
+                Assert.IsType<ResponceExperimentsStatisticDTO.ExperimentStatisticsDto>(result);
+                Assert.Equal(2, result.ExperimentCount);
+                Assert.Equal(3, result.DevicesTotal);
+                Assert.NotNull(result.ExperimentDetails);
+                Assert.Equal(2, result.ExperimentDetails.Count());
+            }
+        }
+
+        private void SampleDatabase(AppDBContext context)
+        {
+            var experiment1 = new Experiments { ExperimentName = "button_color" };
+            var experiment2 = new Experiments { ExperimentName = "price" };
+
+            var device1 = new Devices { DeviceToken = "test4412" };
+            var device2 = new Devices { DeviceToken = "test4413" };
+            var device3 = new Devices { DeviceToken = "test4414" };
+
+            var experimentValue1 = new ExperimentValues { Value = "#FF0000", ProcentValue = 100 };
+            var experimentValue2 = new ExperimentValues { Value = "10", ProcentValue = 100 };
+
+            var deviceExperiment1 = new DeviceExperiments { Device = device1, Experiment = experiment1, ExperimentValue = experimentValue1 };
+            var deviceExperiment2 = new DeviceExperiments { Device = device2, Experiment = experiment1, ExperimentValue = experimentValue2 };
+            var deviceExperiment3 = new DeviceExperiments { Device = device3, Experiment = experiment2, ExperimentValue = experimentValue1 };
+
+            context.AddRange(experiment1, experiment2);
+            context.AddRange(device1, device2, device3);
+            context.AddRange(experimentValue1, experimentValue2);
+            context.AddRange(deviceExperiment1, deviceExperiment2, deviceExperiment3);
+            context.SaveChanges();
+        }
+    }
+}
